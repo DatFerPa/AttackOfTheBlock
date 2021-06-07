e0393b8c6b1207f399172053b7251ce5b466e663
Fernando Palazuelo <fpalazuelo@gmail.com>
2021-06-07 02:16:09 +0200

    more cosiñas

--- AttackOfTheBlock/Assets/Code/Scripts/Managers/PowerUpManger.cs
+++ AttackOfTheBlock/Assets/Code/Scripts/Managers/PowerUpManger.cs
@@ -32,6 +32,7 @@ public class PowerUpManger : MonoBehaviour
     {
         int powerUpIndex = Random.Range(0, powerUpsToSpawn.Count);
         Vector2 spawnPoint = getSpawnPoint();
+        Debug.Log(spawnPoint);
         GameObject powerUpSpawned = Instantiate(powerUpsToSpawn[powerUpIndex], new Vector3(spawnPoint.x, spawnPoint.y, 0), Quaternion.identity ,parentToSpawnPowerUps.transform);
         _powerUpsSpawned.Add(powerUpSpawned);
         powerUpSpawned.GetComponent<Rigidbody2D>().AddForce(getDirectionToCenter(spawnPoint)*movementForceForPowerUps);
@@ -47,7 +48,17 @@ public class PowerUpManger : MonoBehaviour
     private Vector2 getSpawnPoint()
     {
         BoundsAndCameraManager boundsAndCameraManager = GetComponent<BoundsAndCameraManager>();
-        return new Vector2(Random.Range(-1f, 1f) * ((boundsAndCameraManager.widthMainCamera/2)+extraRadius), Random.Range(-1f, 1f) * ((boundsAndCameraManager.heightMainCamera / 2) + extraRadius));
+        int directionX = 0;
+        int directionY = 0;
+        //Vamos a evitar que la dirección sea  0 0
+        do
+        {
+            directionX = Random.Range(-1, 2);
+            directionY = Random.Range(-1, 2);
+        } while (directionX == 0 && directionY == 0);
+
+
+        return new Vector2(directionX * ((boundsAndCameraManager.widthMainCamera/2)+extraRadius), directionY * ((boundsAndCameraManager.heightMainCamera / 2) + extraRadius));
     }
 
     private void destroyPowerUpsUsed()
